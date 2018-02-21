cba493e11d1e4e6e74bf78e9d5380c154d813670
yttan <ytan5@uci.edu>
2018-02-20 19:13:57 -0800

    proj2

--- spacetime-crawler/applications/search/crawler_frame.py
+++ spacetime-crawler/applications/search/crawler_frame.py
@@ -11,15 +11,12 @@ from urlparse import urlparse, parse_qs, urljoin
 from uuid import uuid4
 subdomains = {}
 invalidcount = 0
-maxpage = None
+maxpage = ""
 maxoutcount = 0
 logger = logging.getLogger(__name__)
 LOG_HEADER = "[CRAWLER]"
-TRAP_POOL = {"calendar.ics.uci.edu"}
-#TRAP_POOL = {"calendar.ics.uci.edu","tippersweb.ics.uci.edu/research","www.ics.uci.edu/%%7elab/students/printing.php","www.ics.uci.edu/faculty/area/area_scientific.php",
-#"www.ics.uci.edu/computing/linux/shell.php","www.ics.uci.edu/about/bren/gallery/gallery_15_jpg.php","testlab.ics.uci.edu/data/links.html","www.ics.uci.edu/about/annualreport/index.php"
-#,"www.ics.uci.edu/about/bren/bren_press.php","graphics.ics.uci.edu/gallery.html","www.ics.uci.edu/%%7elab/proj_ics/index.php","www.ics.uci.edu/ugrad/internship/index.php",
-#"cml.ics.uci.edu","jgarcia.ics.uci.edu","www.ics.uci.edu/~lab/students/unixfiles.php"}
+#TRAP_POOL = {"calendar.ics.uci.edu"}
+TRAP_POOL = {"calendar.ics.uci.edu","tippersweb.ics.uci.edu/research","vision.ics.uci.edu/datasets/db.all.sift","www.ics.uci.edu/~kai.zheng","www.ics.uci.edu/%7E","www.ics.uci.edu/%7e","www.ics.uci.edu/~dsm/dyn/release/files/"}
 @Producer(Ytan5Link)
 @GetterSetter(OneYtan5UnProcessedLink)
 @ServerTriggers(add_server_copy, get_downloaded_content)
@@ -42,9 +39,10 @@ class CrawlerFrame(IApplication):
         global invalidcount
         global maxpage
         global maxoutcount
-        unprocessed_links = self.frame.get(OneYtan5UnProcessedLink)
+        unprocessed_links = self.frame.get_new(OneYtan5UnProcessedLink)
         if unprocessed_links:
             link = unprocessed_links[0]
+            
             print "Got a link to download:", link.full_url
             downloaded = link.download()
             myurl = urlparse(link.full_url)
@@ -57,16 +55,17 @@ class CrawlerFrame(IApplication):
                 subdomains[subdomain] = 1
             else:
                 subdomains[subdomain] += 1
+            f = open("result.txt","w")
+            fcontent = ["subdomains: ","\n",str(subdomains),"\n","max out links page:","\n",str(maxpage),"\n",str(maxoutcount),"\n","invalid urls","\n",str(invalidcount)]
+            f.writelines(fcontent)
+            f.close()
             for l in links:
                 if is_valid(l):
                     self.frame.add(Ytan5Link(l))
-                else:
-                    invalidcount += 1
-            f = open("result.txt","w")
-            fcontent = ["subdomains: ","\n",str(subdomains),"\n","max out links page:","\n",str(maxpage),"\n","invalid urls","\n",str(invalidcount)]
-            f.writelines(fcontent)
-            f.close()
-
+                    
+            
+             
+            
     def shutdown(self):
         print (
             "Time time spent this session: ",
@@ -84,15 +83,21 @@ def extract_next_links(rawDataObj):
 
     Suggested library: lxml
     '''
-
+    global invalidcount
     if rawDataObj.http_code != 200 or (rawDataObj.error_message != None and rawDataObj.error_message != '') or rawDataObj.is_redirected:
         return outputLinks
-    page = etree.HTML(rawDataObj.content)
-    hrefs = page.xpath(u"//a")
+    # Credit: https://github.com/gjwGithub/search-engine/blob/master/generateGraph.py
+    webpage = etree.HTML(rawDataObj.content)
+    hrefs = webpage.xpath(u"//a")
     for href in hrefs:
         rawHref = href.get("href")
         absHref = urljoin(rawDataObj.url, rawHref)
-        outputLinks.append(absHref)
+        if is_valid(absHref):
+            if absHref[-1] != "/":
+                absHref += "/"
+            outputLinks.append(absHref)
+        else:
+            invalidcount += 1
     return outputLinks
 
 def is_valid(url):
@@ -110,17 +115,18 @@ def is_valid(url):
             return False
     if url.__contains__('?'):
         return False
+  
     if url.__contains__("../"):
         return False
-    if not (url.endswith(".html") or url.endswith(".xml") or url.endswith(".php")):
-        return False
+    #if not (url.endswith(".html") or url.endswith(".xml") or url.endswith(".php")):
+    #    return False
     parsed = urlparse(url)
     if parsed.scheme not in set(["http", "https"]):
         return False
     try:
         return ".ics.uci.edu" in parsed.hostname \
             and not re.match(".*\.(css|js|bmp|gif|jpe?g|ico" + "|png|tiff?|mid|mp2|mp3|mp4"\
-            + "|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf" \
+            + "|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf|txt|war|apk" \
             + "|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso|epub|dll|cnf|tgz|sha1" \
             + "|thmx|mso|arff|rtf|jar|csv"\
             + "|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
